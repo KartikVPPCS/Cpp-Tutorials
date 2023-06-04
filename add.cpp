d73a382c067cafe4a44a356a2cc77849790a6959
Kartik Patil <107984241+KartikVPPCS@users.noreply.github.com>
2023-06-04 18:27:37 +0530

    Basic of c++
    Today we studied about c++ basics concepts like:
    data types , input out , type casting, operators

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+ int num1,num2,result;
+ int main(){
+
+     cout<<"Enter first number: ";
+     cin>>num1;
+
+     cout<<"Enter second number: ";
+     cin>>num2;
+     result= num1+num2;
+     cout<<"The sum of "<<num1<<" and "<<num2<<" is "<<result<<endl;
+     return 0;
+
+}
---
+++ intro.cpp
@@ -0,0 +1,79 @@
+// Data Types and its size
+#include <iostream>
+using namespace std;
+
+int main(){
+ /*
+    // INT
+    int a = 123;
+    cout<< a << endl;
+    int size0 = sizeof(a);
+    cout<<"size of a is :"<< size0 <<endl;
+
+    // CHAR
+    char b = 'K';
+    cout<< b << endl;
+    int size1 = sizeof(b);
+    cout<<"size of b is :"<< size1 <<endl;
+
+    // BOOL
+    bool bl = true;
+    cout<< bl << endl;
+    int size2= sizeof(bl);
+    cout<<"size of b1 is :"<< size2 <<endl;
+
+    //DOUBLE
+    double d = 1.23;
+    cout<< d << endl;
+    int size3= sizeof(d);
+    cout<<"size of d is :"<< size3 <<endl;
+*/
+
+/*
+    // Type casting
+
+    int a='a';
+    cout<< a << endl;
+
+    char ch=98;
+    cout<< ch << endl;
+
+    // Negative positive numbers
+
+    unsigned int n = -556;
+    cout<< n <<endl;
+
+    signed int m = -556;
+    cout<< m << endl;
+*/
+   // Relation operator
+
+   int a=5;
+   int b=3;
+   cout<<"The first number is: "<< a<<endl;
+   cout<<"The second number is: "<< b << endl;
+    bool first = (a==b);
+    cout<< first << endl;
+
+    bool second = (a>b);
+    cout<< second << endl;
+
+    bool third = (a<b);
+    cout<< third << endl;
+
+    bool forth = (a>=b);
+    cout<< forth << endl;
+
+    bool fifth = (a<=b);
+    cout<< fifth << endl;
+
+    bool sixth = (a!=b);
+    cout<< first << endl;
+
+
+
+
+
+
+
+}
